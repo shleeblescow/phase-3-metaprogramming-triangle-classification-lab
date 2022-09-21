fa6013e45392777cf3490698c562f4e90db1c20a
Shelby Lee <slee337@uw.edu>
2022-09-21 15:49:15 -0600

    commit tree c1de83cf9a8e9f7454263506090b53e3e0c96369

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  # triangle code
+
+  attr_reader :a, :b, :c
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if a == 0 && b == 0 && c == 0
+      "triangle error no size"
+      raise TriangleError
+    elsif a < 0 || b < 0 || c < 0
+      "triangel error negative sides"
+      raise TriangleError
+    elsif a + b <= c || a + c <= b || b + c <= a
+      "triangle error violates triangle inequality"
+      raise TriangleError
+    end
+
+    if a == b && a == c
+      :equilateral
+    elsif a == b || a == c || b == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
